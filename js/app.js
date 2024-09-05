fbeddc471dc9b83be212e8f0a0660f8244aaeac7
Axel <20233tn131@utez.edu.mx>
2024-09-04 20:55:12 -0600

    Inicio del proyecto

---
+++ js/app.js
@@ -0,0 +1,4 @@
+document.getElementById('btnCambiarColor').addEventListener('click', function() {
+  var parrafo = document.getElementById('descripcion');
+  parrafo.style.backgroundColor = '#ffc107';  // Cambia el color de fondo a amarillo
+});
